f97488334f5f297e0e2b7617fbe09845a30aaba6
mahamudul hasan seber <sebermh@gmail.com>
2024-09-24 01:26:58 +0600

    add button challenging part

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-console.log('start')
+// console.log('start')
+
+// add button challenging part
+
+
+function goToPage2() {
+    window.location.href = 'blog.html';
+
+};
+document.getElementById('my-button').addEventListener('click', goToPage2)
+
+document.getElementById('my-button').addEventListener('click', function () {
+    window.history.back();
+});
+
+
+
+
