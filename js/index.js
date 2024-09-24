64ca525a309d8060e2fb55667960de14512c6a96
mahamudul hasan seber <sebermh@gmail.com>
2024-09-24 19:32:25 +0600

    add

--- js/index.js
+++ js/index.js
@@ -28,6 +28,88 @@ function changeColor(buttonId) {
 }
 
 
+// donate calculate one
+
+document.getElementById('btn-add-money')
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+
+        const addMoney = document.getElementById('donationInput').value;
+        const addMoneyNumber = parseFloat(addMoney);
+        // const pinNumber = document.getElementById('input-pin-number').value;
+        console.log(addMoney);
+
+
+        if (addMoney) {
+            const balance = document.getElementById('account-balance').innerText;
+            const balanceNumber = parseFloat(balance)
+
+            const newBalance = balanceNumber + addMoneyNumber;
+            document.getElementById('account-balance').innerText = newBalance;
+        }
+        else {
+            alert('failed to Donate Money')
+        }
+
+
+
+    })
+
+// donate calculate two
+
+
+document.getElementById('btn-add-money2')
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+
+        const addMoney = document.getElementById('donationInput2').value;
+        const addMoneyNumber = parseFloat(addMoney);
+        // const pinNumber = document.getElementById('input-pin-number').value;
+        console.log(addMoney);
+
+
+        if (addMoney) {
+            const balance = document.getElementById('account-balance2').innerText;
+            const balanceNumber = parseFloat(balance)
+
+            const newBalance = balanceNumber + addMoneyNumber;
+            document.getElementById('account-balance2').innerText = newBalance;
+        }
+        else {
+            alert('failed to Donate Money')
+        }
+    })
+
+// donate calculate three
+
+document.getElementById('btn-add-money3')
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+
+        const addMoney = document.getElementById('donationInput3').value;
+        const addMoneyNumber = parseFloat(addMoney);
+        // const pinNumber = document.getElementById('input-pin-number').value;
+        console.log(addMoney);
+
+
+        if (addMoney) {
+            const balance = document.getElementById('account-balance3').innerText;
+            const balanceNumber = parseFloat(balance)
+
+            const newBalance = balanceNumber + addMoneyNumber;
+            document.getElementById('account-balance3').innerText = newBalance;
+        }
+        else {
+            alert('failed to Donate Money')
+        }
+
+
+
+    })
+
+
+
+
 
 
 
