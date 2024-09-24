90327115d75588eb85e47d582391e3d01779948c
mahamudul hasan seber <sebermh@gmail.com>
2024-09-24 15:52:30 +0600

    banner button change color

--- js/index.js
+++ js/index.js
@@ -13,6 +13,21 @@ document.getElementById('my-button').addEventListener('click', function () {
     window.history.back();
 });
 
+// button change color
+
+let activeButton = null;
+
+function changeColor(buttonId) {
+    if (activeButton) {
+        document.getElementById(activeButton).classList.remove('bg-[#B4F461]');
+        document.getElementById(activeButton).classList.add('bg-gray-200');
+    }
+    activeButton = buttonId;
+    document.getElementById(buttonId).classList.remove('bg-gray-200');
+    document.getElementById(buttonId).classList.add('bg-[#B4F461]');
+}
+
+
 
 
 
